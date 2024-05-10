a205bdcc4b96d18a79d78331d6a4053a7c6681c3
Elizabeth Agunroye <Elizabethagunroye@gmail.com>
2024-05-10 06:10:39 +0100

    Final submission
    Added approximation to the JavaScript.
    Removed read only from the input for flexibility so user can easily type , rather than press the button

--- script.js
+++ script.js
@@ -9,17 +9,23 @@ function appendToOutput(input){
 // add an eval function for when the equals button is clicked.  
 // If the expression is valid, it should replace the output value with the result of the arithmetic. 
 // If the expression is invalid, should display "Error" in the output
-function result(){
+function result() {
+    try {
+        let result = eval(output.value);
 
-    try{
-        output.value = eval(output.value);
-    }
+        // Check if result is an approximation
+        if (Math.abs(result - Math.round(result)) < 0.0001) {
+            // If it's close to an integer value, round it
+            result = Math.round(result);
+        } else {
+            // If it's not close to an integer, round to 4 decimal places
+            result = result.toFixed(4);
+        }
 
-    catch(error){
-        output.value= "Error";
+        output.value = result;
+    } catch (error) {
+        output.value = "Error";
     }
-    
 }
-
 
 
